4f6a33b384d5c62ea06ee3feeb64ec85f5d99a2b
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2025-08-21 12:51:18 +0300

    Complete exercise 1.2

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,12 @@
+import Part from "./Part";
+
 function Content(props) {
   return (
-    <ul>
-      <li className="flex">
-        <span className="min-w-[300px]">{props.part1}</span>
-        <span>{props.exercises1}</span>
-      </li>
-      <li className="flex">
-        <span className="min-w-[300px]">{props.part2}</span>
-        <span>{props.exercises2}</span>
-      </li>
-      <li className="flex">
-        <span className="min-w-[300px]">{props.part3}</span>
-        <span>{props.exercises3}</span>
-      </li>
-    </ul>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   );
 }
 
