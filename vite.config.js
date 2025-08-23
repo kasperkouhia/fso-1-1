d7e399b140894be8d5f6f86c08334992ae72aedb
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2025-08-23 23:23:23 +0300

    Remove unnecessary base config option

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/fso-part-1/",
   plugins: [react(), tailwindcss()],
 });
